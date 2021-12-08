7881133dc253793412edbe4dc557dfca827905be
elthondsaraujo <78942062+elthondsaraujo@users.noreply.github.com>
2021-12-08 11:26:33 -0300

    .

---
+++ vod/downloader.py
@@ -0,0 +1,141 @@
+from __future__ import division, print_function
+
+import concurrent.futures
+import logging
+import os
+import re
+import sys
+from collections import OrderedDict
+from session import session_factory
+
+if sys.version_info.major == 2:
+    import urlparse  # Python 2.x
+else:
+    import urllib.parse as urlparse  # Python 3.x
+
+import requests
+
+# noinspection PyPackageRequirements
+import slugify
+
+
+class Downloader:
+    @staticmethod
+    def filter_filename_part(string, allowed_pattern=re.compile(r"[^-.@!\w]+", re.IGNORECASE)):
+        """
+        Replace forbidden filename chars to '_' for string
+        :rtype: Text
+        :type string: Text
+        :param allowed_pattern:
+        """
+        result = slugify.slugify(string, lowercase=False, regex_pattern=allowed_pattern)
+        return result
+
+    def __init__(self, download_dir, http_settings=None):
+        self._download_dir = download_dir
+        self._downloaded_files_by_uri = OrderedDict()
+        self._http_session = session_factory(http_settings or {})
+        self._executor = concurrent.futures.ThreadPoolExecutor()
+
+    @property
+    def download_dir(self):
+        return self._download_dir
+
+    def downloaded_files_by_url(self):
+        return self._downloaded_files_by_uri.copy()
+
+    def uri_to_filename(self, absolute_uri):
+        """
+        :param absolute_uri:
+        :return: DOWNLOAD_DIR + hostname + rel_uri with os.path.sep
+        """
+        url_parts = urlparse.urlparse(absolute_uri)
+        rel_filename_parts = [url_parts.netloc] + [i for i in url_parts.path.split("/")[1:] if i]
+        filtered_parts = [self.filter_filename_part(p) for p in rel_filename_parts]
+        rel_filename = os.path.sep.join(filtered_parts)
+        filename = os.path.join(self._download_dir, rel_filename)
+        return filename
+
+    def url_and_file_size_diff(self, uri, filename):
+        """
+        Check filename size on server by Content-Length
+        :return:
+        :type uri: Text
+        :type filename: Text
+        :rtype: int or None
+        """
+        resp = self._http_session.head(uri)
+        header_size_str = resp.headers.get("Content-Length")
+        if header_size_str is None:
+            logging.warning("No 'Content-Length' header for %s", uri)
+            return None
+        uri_size = int(header_size_str)
+        file_size = os.path.getsize(filename)
+        return uri_size - file_size
+
+    def _retrieve_uri_to_file(self, uri, filename):
+        """
+        Download URI to File
+        :type uri: Text
+        :type filename: Text
+        :rtype: None
+        """
+        # Downloading
+        logging.info("Downloading %s to %s", uri, filename)
+        try:
+            resp = self._http_session.get(uri)
+        except requests.RequestException as e:
+            logging.exception(e)
+            raise e
+        # Write to file
+        with open(filename, "wb") as fd:
+            for chunk in resp.iter_content(chunk_size=2 ** 20):
+                fd.write(chunk)
+
+    def download_one_file(self, absolute_uri):
+        """
+        Downloads one file from absolute_uri to DOWNLOAD_DIR + absolute_uri.path
+        if file already exists - skip it
+        :type absolute_uri: Text
+        :rtype: Text
+        """
+        filename = self.uri_to_filename(absolute_uri)
+        if filename == self._downloaded_files_by_uri.get(absolute_uri):
+            # In case of #EXT-X-BYTERANGE for same file
+            logging.warning("File %s already downloaded", filename)
+            return filename  # We already downloaded this file in current session
+        if os.path.isfile(filename):
+            size_diff = self.url_and_file_size_diff(absolute_uri, filename)
+            if size_diff == 0:
+                self._downloaded_files_by_uri[absolute_uri] = filename
+                logging.warning("File %s already exists and same size", filename)
+                return filename  # Same file already exists
+            if size_diff is not None:  # None means not Content-Length header present
+                logging.warning("File %s and URI %s size mismatch", filename, absolute_uri)
+        dst_dir = os.path.dirname(filename)
+        if not os.path.isdir(dst_dir):
+            os.makedirs(dst_dir)
+        self._retrieve_uri_to_file(absolute_uri, filename)
+        self._downloaded_files_by_uri[absolute_uri] = filename
+        logging.info("Downloaded %s -> %s", absolute_uri, filename)
+        return filename
+
+    def download_many(self, segments):
+        """Downloads many segments and waits for completion"""
+        futures = []
+        for segment in segments:
+            future = self._executor.submit(
+                self.download_one_file, segment.absolute_uri)
+            futures.append(future)
+
+        for future in futures:
+            future.result()
+
+
+def test():
+    downloader = Downloader(download_dir=".")
+    downloader.download_one_file("http://tungsten.aaplimg.com/VOD/bipbop_adv_example_hevc/master.m3u8")
+
+
+if __name__ == "__main__":
+    test()
---
+++ vod/main.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+
+from __future__ import division, print_function
+
+import argparse
+import codecs
+import json
+import logging
+import os
+import shutil
+import sys
+from collections import defaultdict
+
+if sys.version_info.major == 2:
+    import urlparse  # Python 2.x
+else:
+    import urllib.parse as urlparse  # Python 3.x
+
+import m3u8
+
+import downloader
+
+
+class HlsDownloaderException(Exception):
+    pass
+
+
+DOWNLOADER = None  # Instance of downloader.Downloader
+
+DESCRIPTION = defaultdict(list)
+
+
+def download_files_from_playlist(m3u8list):
+    """
+    Get all content from m3u8 playlist
+    :type m3u8list: m3u8.M3U8
+    :rtype: None
+    """
+    for media in m3u8list.media:
+        if media.absolute_uri:
+            DESCRIPTION["MEDIA." + media.type].append(media.absolute_uri)
+            process_playlist_by_uri(media.absolute_uri)
+
+    for iframe_playlist in m3u8list.iframe_playlists:
+        if iframe_playlist.absolute_uri:
+            DESCRIPTION["IFRAME-STREAMS"].append(iframe_playlist.absolute_uri)
+            process_playlist_by_uri(iframe_playlist.absolute_uri)
+
+    if m3u8list.playlists:
+        for playlist in m3u8list.playlists:
+            if not playlist.absolute_uri.startswith(playlist.base_uri):
+                logging.warning("Base URI changed from %s to %s", playlist.base_uri, playlist.absolute_uri)
+            process_playlist_by_uri(playlist.absolute_uri)
+        return
+
+    # noinspection PyUnresolvedReferences
+    if not m3u8list.is_endlist:
+        raise HlsDownloaderException("Playlist is not endlist. Only VOD Playlists are supported")
+
+    segment_map_absolute_url = None
+    if m3u8list.segment_map:
+        segment_map_absolute_url = urlparse.urljoin(m3u8list.base_uri, m3u8list.segment_map["uri"])
+    if segment_map_absolute_url:
+        DOWNLOADER.download_one_file(segment_map_absolute_url)
+
+    DOWNLOADER.download_many(m3u8list.segments)
+
+
+def process_playlist_by_uri(absolute_uri):
+    """
+    Download and process m3u8 playlist
+    :type absolute_uri: Text
+    :return: Filename of downloaded Playlist
+    :rtype: Text
+    """
+    filename = DOWNLOADER.download_one_file(absolute_uri)
+    base_uri = "/".join(absolute_uri.split("/")[:-1]) + "/"
+
+    with codecs.open(filename, mode="rb", encoding="utf-8") as pl_f:
+        pl_content = pl_f.read().strip()
+    media_playlist = m3u8.M3U8(content=pl_content, base_uri=base_uri)
+
+    download_files_from_playlist(media_playlist)
+    return filename
+
+
+def process_main_playlist(url_to_m3u8):
+    """
+    Process main playlist and save it to main.m3u8
+    Additional information to description.json
+    :type url_to_m3u8: Text
+    :rtype: None
+    """
+    DESCRIPTION["origin_url"] = url_to_m3u8
+
+    main_list_filename = process_playlist_by_uri(url_to_m3u8)
+
+    downloaded_files = DOWNLOADER.downloaded_files_by_url()
+    for u in downloaded_files:
+        downloaded_files[u] = os.path.relpath(downloaded_files[u], DOWNLOADER.download_dir)
+    DESCRIPTION["Files"] = downloaded_files
+
+    main_list_dir = os.path.dirname(main_list_filename)
+    description_filename = os.path.join(main_list_dir, "description.json")
+    with codecs.open(description_filename, mode="wb", encoding="utf-8") as jf:
+        json.dump(DESCRIPTION, jf, indent=2, separators=(",", ": "))
+    logging.info("Description saved to %s", description_filename)
+
+    main_list_copy_filename = os.path.join(main_list_dir, "main.m3u8")
+    if not os.path.isfile(main_list_copy_filename):
+        shutil.copy(main_list_filename, main_list_copy_filename)
+        logging.info("Copied %s -> %s", main_list_filename, os.path.join(main_list_dir, "main.m3u8"))
+
+
+def main(url_to_m3u8, download_dir, verbose, ignore_ssl):
+    """
+    :type url_to_m3u8: str
+    :type download_dir: str
+    :type verbose: bool
+    :type ignore_ssl: bool
+    :rtype: None
+    """
+    http_settings = dict(
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)"
+            " AppleWebKit/602.4.8 (KHTML, like Gecko)"
+            " Version/10.0.3 Safari/602.4.8"
+        },
+    )
+    if ignore_ssl:
+        http_settings["verify"] = False
+
+    global DOWNLOADER
+    DOWNLOADER = downloader.Downloader(download_dir=download_dir, http_settings=http_settings,)
+    logging.basicConfig(level=logging.INFO if verbose else logging.WARNING)
+
+    process_main_playlist(url_to_m3u8)
+
+
+def parse_args():
+    """
+    :rtype: dict
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url_to_m3u8", help="Url to main.m3u8")
+    parser.add_argument("download_dir", help="Path to save files")
+    parser.add_argument("--ignore-ssl", default=False, action="store_true", help="Ignore SSL verification")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Be more verbose")
+    kwargs = vars(parser.parse_args())
+    return kwargs
+
+
+if __name__ == "__main__":
+    main(**parse_args())
